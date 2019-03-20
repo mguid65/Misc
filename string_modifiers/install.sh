ada8532c59ff71b0b0150044c88443ed8116a1e8
Matthew Guidry <Matthew Guidry>
2019-03-20 09:47:44 -0500

    Add string modifiers

---
+++ string_modifiers/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x tolower
+chmod +x toupper
+chmod +x flipcase
+
+cp tolower /usr/local/bin/tolower
+cp toupper /usr/local/bin/toupper
+cp flipcase /usr/local/bin/flipcase
+
+echo done
---
+++ string_modifiers/src/flipcase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+
+int main(int argc, const char * argv[]) {
+  if(argc < 2 ) {
+    cout << "USAGE: " << argv[0] << " <string>\n";
+    exit(1);
+  }
+
+  string input(argv[1]);
+  for(char & c: input) {
+    cout << (char) ((c > 64 && c < 123) ? c^32 : c);
+  }
+
+  cout << '\n';
+}
